017e6814c94e2f5641137ab646dce2b040408bd3
avnikasikciwin <avni.kasikci.01@gmail.com>
2022-09-23 13:26:43 +0300

    insert : mongoRunner

--- ContactApp.Module.Report.Test/ReportServiceTest.cs
+++ ContactApp.Module.Report.Test/ReportServiceTest.cs
@@ -5,22 +5,49 @@ using ContactApp.Core.Persistence.Repository;
 using ContactApp.Module.Report.Application.Domain;
 using ContactApp.Module.Report.Application.Services;
 using ContactApp.Module.Report.Application.Services.Interfaces;
+using Mongo2Go;
+using MongoDB.Driver;
 using System.Linq;
 using Xunit;
 
 namespace ContactApp.Module.Report.Test
 {
+    //public class MongoIntegrationTest
+    //{
+    //    internal static MongoDbRunner _runner;
+    //    internal static MongoCollection<TestDocument> _collection;
 
+    //    internal static void CreateConnection()
+    //    {
+    //        _runner = MongoDbRunner.Start();
+
+    //        MongoClient client = new MongoClient(_runner.ConnectionString);
+    //        MongoDatabase database = client.GetDatabase("IntegrationTest");
+    //        _collection = database.GetCollection<TestDocument>("TestCollection");
+    //    }
+    //}
     public class ReportServiceTest
     {
         private readonly IReportService _reportService;
+        private readonly MongoDbRunner _runner;
+        //internal static MongoDbRunner _runner;
+
         public ReportServiceTest()
         {
-            var DbProvider = new MongoDbSettings
-            { Collection = "", ConnectionString = "mongodb://localhost:27017", Database = "DbContactReport" };
-            var repositoryBudgetExpense = new MongoDbRepository<EntityReport>(DbProvider);
+            _runner = MongoDbRunner.StartForDebugging();
+            _runner.Import("DbContactReport", "DbContactReport", @"..\..\App_Data\DbContactReport.json", true);
 
-            _reportService = new ReportService(repositoryBudgetExpense);
+            var connStr = _runner.ConnectionString;
+            var DbProvider = new MongoDbSettings
+            { 
+                Collection = "",
+                //ConnectionString = "mongodb://localhost:27017", 
+                ConnectionString = connStr, 
+                Database = "DbContactReport" };
+            var mongoDbRepository = new MongoDbRepository<EntityReport>(DbProvider);
+
+            _reportService = new ReportService(mongoDbRepository);
+
         }
         #region Property  
         //public Mock<IReportService> mockReportService = new Mock<IReportService>();
